e8e8f769c429107e2b43b99942a01c4114617d60
abouguri <abouguri>
2025-11-21 15:55:37 +0100

    Fix: Remove unused import in task_detail_screen.dart
    - Removed unused Task model import
    - Code passes dart analyze with no issues

--- lib/screens/task_detail_screen.dart
+++ lib/screens/task_detail_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import '../models/task.dart';
 import '../providers/task_provider.dart';
 import 'add_edit_task_screen.dart';
 
